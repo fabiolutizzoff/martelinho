98d8016de733dbb7484d9de95da441826f9feec7
Fabio Lutizzoff <fabiolutizzoff@gmail.com>
2022-05-24 18:41:27 -0300

    Primeiro

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar style="background-color: white">
+        <q-toolbar-title>
+          <q-img
+            src="~assets/logo-sanxs.png"
+            spinner-color="white"
+            style="max-width: 150px; display: block; margin-left: auto; margin-right: auto"
+          />
+        </q-toolbar-title>
+        <div><q-btn round color="brown-5" icon="directions" /></div>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+</script>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="q-pa-md row items-start q-gutter-md">
+      <q-card-actions vertical>
+        <q-input v-model="email" filled type="email" label="Email" />
+        <q-input v-model="password" filled :type="isPwd ? 'password' : 'text'" label="senha">
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+          </template>
+        </q-input>
+        <q-btn color="primary" label="LOGIN" />
+        <q-btn flat color="primary" label="Esqueci minha senha" />
+      </q-card-actions>
+    </div>
+  </q-page>
+</template>
+
+<script>
+
+</script>
